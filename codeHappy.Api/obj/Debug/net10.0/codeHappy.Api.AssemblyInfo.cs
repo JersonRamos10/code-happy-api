055e21203afec1ed03b22b6d95fd504548a15edb
Jerson Ramos <jerson_ramos1310@yahoo.es>
2026-06-15 15:12:07 -0600

    chrone: remove obj and .idea folders from tracking and update gitignore

--- codeHappy.Api/obj/Debug/net10.0/codeHappy.Api.AssemblyInfo.cs
+++ codeHappy.Api/obj/Debug/net10.0/codeHappy.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("codeHappy.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6489078579133dc4476ac5264288f69414463ce1")]
 [assembly: System.Reflection.AssemblyProductAttribute("codeHappy.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("codeHappy.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
